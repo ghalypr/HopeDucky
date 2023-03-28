706704686096e9058c2a325a837d7914095f81cf
عبدالمنعم غالى <ghalypr@gmail.com>
2023-03-28 16:42:46 +0200

    Add files via upload

---
+++ include/keys.h
@@ -0,0 +1,17 @@
+#include <windows.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include<sstream>
+
+#include "./String2WString.h"
+
+
+
+
+void PressKeys(string str);
+
+void WinKey();
+
+void ReturnKey();
