30bb6f7ea2e4af642f6532694fcf684c5bea9677
عبدالمنعم غالى <ghalypr@gmail.com>
2023-03-29 03:42:59 +0200

    Add files via upload

--- HopeDucky.cpp
+++ HopeDucky.cpp
@@ -1,28 +1,20 @@
 #include "include/HopeDucky.h"
 
-
 using namespace std;
-
 
 int main(){
 
-
-  WinKey();
-  Sleep(500);
-  PressKeys("run");
-  Sleep(500);
-  ReturnKey();
-  Sleep(500);
-  PressKeys("powershell");
-  Sleep(500);
-  ReturnKey();
-  Sleep(500);
-  PressKeys("echo 'Hello World From HopeDucky!'");
-  Sleep(500);
-  ReturnKey();
-
-
-
+    GUIR();
+    
+    Sleep(500);
+    PressKeys("powershell");
+    Sleep(500);
+    ReturnKey();
+    Sleep(500);
+    PressKeys("echo 'Hello World From HopeDucky!'");
+    Sleep(500);
+    ReturnKey();
+    
 
     return 0x0;
 }
